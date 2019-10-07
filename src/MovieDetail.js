98858b056b88569e1b5cc07c28d886844ac0f4bd
Franz Casabuena <franzneandra@gmail.com>
2019-10-07 12:48:51 +0800

    project finished. just adding to repository and deploying

---
+++ src/MovieDetail.js
@@ -0,0 +1,75 @@
+/* eslint react/no-did-mount-set-state: 0 */
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Overdrive from 'react-overdrive';
+import { Poster } from './Movie';
+
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
+const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280';
+
+class MoviesList extends Component {
+  state = {
+    movie: [],
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=4d94c1221a30b9ec610b24580491d9c1&language=en-US`);
+      const movie = await res.json();
+      console.log(movie);
+      this.setState({
+        movie,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    const { movie } = this.state;
+    return (
+      <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
+        <MovieInfo>
+          <Overdrive id={movie.id}>
+            <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+          </Overdrive>
+          <div>
+            {movie.title ? (
+              <h1>Hello</h1>
+              ) : (
+                <h1>Hi?</h1>
+              )}
+            <h1>{movie.title}</h1>
+            <h3>{movie.release_date}</h3>
+            <p>{movie.overview}</p>
+          </div>
+        </MovieInfo>
+      </MovieWrapper>
+    );
+  }
+}
+
+
+export default MoviesList;
+
+const MovieWrapper = styled.div`
+    position: relative;
+    padding-top: 50vh;
+    background: url(${props => props.backdrop}) no-repeat;
+    background-size: cover;
+`;
+
+
+const MovieInfo = styled.div`
+    background: white;
+    text-align:left;
+    padding: 2rem 10%;
+    display: flex;
+    > div {
+        margin-left: 20px;
+    }
+    img {
+        position: relative;
+        top: -5rem;
+    }
+`;
